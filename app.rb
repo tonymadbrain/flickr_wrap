1bdd1e88b0ce60f169242f9e5662d3201b741c66
Antony Ryabov <mb7222@yandex.ru>
2016-06-07 17:43:29 +0300

    Added some AJAX. Working button Sync

--- app.rb
+++ app.rb
@@ -1,5 +1,4 @@
 require 'sinatra'
-require 'sinatra/reloader'
 require 'redis'
 require 'flickraw'
 require 'json'
@@ -15,16 +14,18 @@ configure :test do
 end
 
 configure :development do
+  require 'sinatra/reloader'
+  register Sinatra::Reloader
+  set :bind, '0.0.0.0'
 end
 
-# Create redis object with connection
 redis = Redis.new
 
 FlickRaw.api_key       = ENV['FLICKR_API_KEY']
 FlickRaw.shared_secret = ENV['FLICKR_SHARED_SECRET']
 flickr.access_token    = ENV['FLICKR_ACCESS_TOKEN']
 flickr.access_secret   = ENV['FLICKR_ACCESS_SECRET']
-user                   = ENV['FLICKR_USER']
+user                  = ENV['FLICKR_USER']
 
 login = flickr.test.login
 puts "You are now authenticated as #{login.username}"
@@ -34,11 +35,14 @@ get '/' do
   redis.keys("flickr_*").each do |key|
     @images << JSON.parse(redis.get("#{key}"))
   end
-  slim :index
+  slim :images, layout: :index
 end
 
-get '/sync' do
-  photos = flickr.photos.search(user_id: user, per_page: 500)
+post '/sync' do
+  return 400, {"message": "LOL!"}.to_json
+  per_page = params['count']
+  per_page ||= 10
+  photos = flickr.photos.search(user_id: user, per_page: per_page)
   photos.each do |photo|
     info = flickr.photos.getInfo(photo_id: photo.id)
     url = FlickRaw.url_o(info)
@@ -46,5 +50,5 @@ get '/sync' do
     hash = {id: photo.id, filename: info['title'], url: url, preview: url_small}
     redis.set("flickr_#{info['title']}", hash.to_json)
   end
-  return 200, "DONE"
+  return 200, {"status": "OK"}.to_json
 end
---
+++ public/js/custom.js
@@ -0,0 +1,43 @@
+$(function (){
+  $('#delete_link').on('click', function(e){
+   var check_item = $('input:checked','#delete_form'),
+   _arr = [];
+    e.preventDefault();
+    check_item.each(function(item, index){
+      _arr.push($(index).prop('value'));
+    });
+    $.ajax({
+      type: "POST",
+      url: "/delete",
+      data: {"checkbox":_arr},
+      dataType: "html",
+      success: function(data) {
+        $.map(_arr, function(value, index) {
+          document.getElementById([value]).remove();
+        });
+        $('#alerts').html(data);
+      },
+      error: function(xhr, status, error) {
+        $('#alerts').html("Some errors: " + error)
+      }
+    });
+  })
+  $('#sync_link').on('click', function(e){
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "/sync",
+      // data: {"update":true},
+      // dataType: "html",
+      success: function() {
+        // var response = JSON.parse ( data );
+        // $('#alerts').html(response.status);
+        location.reload();
+      },
+      error: function(xhr, status, error) {
+        var err = JSON.parse(xhr.responseText)
+        $('#alerts').html("<div class='alert alert-error'>"+err.message+"</div>")
+      }
+    });
+  })
+});
